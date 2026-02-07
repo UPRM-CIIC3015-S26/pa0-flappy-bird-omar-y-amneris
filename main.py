c737bdbb69e0ae92fc5544a54c5676929964b3c8
amneristorres4 <amneris.torres4@upr.edu>
2026-02-07 16:36:31 -0400

    Completed task 5

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ pipe_width = 70
 # TODO 2.1: A Little gap Problem
 # You probably noticed when running the code that it's impossible the player to go through the gaps
 # play around with the pipe_gap variable so that its big enough for the player to pass through
-pipe_gap = 151
+pipe_gap = 150
 pipe_height = random.randint(100, 400)
 # TODO 2.2: The too fast problem
 # The pipes are moving way too fast! Play around with the pipe_speed variable until you find a good
@@ -109,7 +109,7 @@ while running:
         if bird_rect.colliderect(top_pipe_rect) or bird_rect.colliderect(bottom_pipe_rect):
             game_over = True
 
-    screen.fill(pygame.Color('grey12'))
+    screen.fill(pygame.Color('dodgerblue4'))
     # TODO 5: A Bird's Color
     # The color of the player is currently white, let's change that a bit! You are free to change the bird's
     # to whatever you wish. You will need to head back to where the PLAYER variable was created and change the values.
